ee7b7e36fbbf18cbd0dda5ce3d83669bf243b6ad
turtledev <turtledev65@gmail.com>
2023-08-13 00:56:00 +0300

    New update. Minor UI changes. New confirmation modal for deleting lists that are not empty. Change the split item badge

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,7 +10,7 @@ self.addEventListener("install", (event) => {
       console.log("Opened cache");
 
       return cache.addAll(urlsToCache);
-    })
+    }),
   );
 });
 
@@ -19,7 +19,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((res) => {
       return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
+    }),
   );
 });
 
@@ -34,8 +34,8 @@ self.addEventListener("activate", (event) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
-        })
-      )
-    )
+        }),
+      ),
+    ),
   );
 });
